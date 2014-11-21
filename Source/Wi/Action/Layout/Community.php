c1795d0e567606193d8453ab67a7b6c0e3768a1b
dp <das@landev.ru>
2014-11-21 20:58:33 +0300

    some important fixes

--- Source/Wi/Action/Layout/Community.php
+++ Source/Wi/Action/Layout/Community.php
@@ -14,41 +14,6 @@ use Ice\Core\Action_Context;
  * @author dp
  * @version stable_0
  */
-class Layout_Community extends Layout
+class Layout_Community extends Layout_Main
 {
-    /**  public static $config = [
-     *      'afterActions' => [],          // actions
-     *      'layout' => null,               // Emmet style layout
-     *      'template' => null,             // Template of view
-     *      'output' => null,               // Output type: standart|file
-     *      'viewRenderClassName' => null,  // Render class for view (example: Ice:Php)
-     *      'inputDefaults' => [],          // Default input data
-     *      'inputValidators' => [],        // Input data validators
-     *      'inputDataProviderKeys' => [],  // InputDataProviders keys
-     *      'outputDataProviderKeys' => [], // OutputDataProviders keys
-     *      'cacheDataProviderKey' => ''    // Cache data provider key
-     *  ];
-     */
-    public static $config = [
-        'afterActions' => [
-            'Ice:Resources',
-            'Ice:Menu' => [
-                'scheme' => [
-                    'Документация' => [
-                        'Руководство (Handbook)' => '/handbook',
-                        'Полезные статьи (Cookbook)' => '/cookbook',
-                        'Исходный код (Api)' => '/resource/api/Ice/0.0',
-                        'Часто задаваемые вопросы (Faq)' => '/faq'
-                    ],
-                    'Сообщество' => [
-                        'Блог' => '/blog',
-//                        'Форум' => '/forum'
-                    ],
-//                    'Приложения' => '/apps',
-                ]
-            ]
-        ],
-        'viewRenderClassName' => 'Ice:Smarty',
-        'layout' => ''
-    ];
 }
